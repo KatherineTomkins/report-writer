69044d75aeed1175b8b97c5df4fcbd67f0a4dc6b
Katherine Tomkins <katherine.tomkins@metoffice.gov.uk>
2019-01-02 21:41:54 +0000

    lower case method name

--- src/main/java/student/report/evals/Orderer.java
+++ src/main/java/student/report/evals/Orderer.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class Orderer {
 
-    public Map<String, Integer> Order(final Map<String, Integer> countedScores) {
+    public Map<String, Integer> order(final Map<String, Integer> countedScores) {
         return countedScores.entrySet().stream().sorted((Map.Entry.<String, Integer> comparingByValue()))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
     }
